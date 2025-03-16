471a160d82474e7a53937b4581f78207e99bca05
MiraMakh <mira.makharashvili@gmail.com>
2025-03-16 20:28:34 +0400

    Add module 1 practical tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import React, { useState } from "react";
+import Counter from "./components/Counter/Counter";
+import SearchForm from "./components/SearchForm/SearchForm";
+import Genres from "./components/Genres/Genres";
 import "./App.scss";
 
-function App() {
-  return <></>;
-}
+const App: React.FC = () => {
+  // @TODO Add layout components and move SearchForm and Genres in their components
+
+  const handleSearch = (searchQuery: string) => {
+    // @TODO search logic here
+  };
+
+  const genres = ["All", "Documentary", "Comedy", "Horror", "Crime"];
+  const [selectedGenre, setSelectedGenre] = useState<string>(genres[0]);
+  const handleGenreSelect = (genre: string) => {
+    setSelectedGenre(genre);
+    console.log("Selected Genre:", genre);
+  };
+
+  return (
+    <>
+      {React.createElement(Counter, { initialValue: 10 })}
+
+      <SearchForm
+        InitialSearchQuery="Initial search Value"
+        onSearch={handleSearch}
+      />
+
+      <Genres
+        genres={genres}
+        selectedGenre={selectedGenre}
+        onSelect={handleGenreSelect}
+      />
+    </>
+  );
+};
 
 export default App;
